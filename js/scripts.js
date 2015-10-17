995a3bd578a157e10cd63a8f7233ba03ed201684
NathanRomike <nathanromike@gmail.com>
2015-10-16 21:59:02 -0700

    Correct output, append, and closing issue.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,19 @@
 $(function() {
 
-  var stopInput = parseInt(prompt("What number would you like to ping-pong up to?"));
+  var endInput = parseInt(prompt("What number would you like to ping-pong up to?"));
 
-  for (var index = 1; index <= stopInput; index ++) {
+  for (var index = 1; index <= endInput; index ++) {
     if (index % 15 === 0) {
-      $("ul.output").append("<li>ping-pong</li>");
+      $("ul").append("<li>ping-pong</li>");
     }
     else if (index % 3 === 0) {
-      $("ul.output").append("<li>ping</li>");
+      $("ul").append("<li>ping</li>");
     }
     else if (index % 5 === 0) {
-      $("ul.output").append("<li>pong</li>");
+      $("ul").append("<li>pong</li>");
     }
     else {
-      $("ul.output").append("<li>" + index + "</li>");
+      $("ul").append("<li>" + index + "</li>");
     }
   }
 });
-
-// To tell if one number is divisible by another number, use the % operator. For example, if a variable called number is divisible by 7, then number % 7 returns 0.
