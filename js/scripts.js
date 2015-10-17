5fcbda8330db29692fc92106bf0bdef364c6446e
NathanRomike <nathanromike@gmail.com>
2015-10-16 21:17:25 -0700

    Add ping and pong output.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
-$(function(){
+$(function() {
 
-  var stop = parseInt(prompt("What number would you like to ping-pong up to?"));
+  var stopInput = parseInt(prompt("What number would you like to ping-pong up to?"));
 
-  var number = ["stop"].indexOf
-
-  for (var index = 1; index <= .stop; index += 1) {
-    $("ul").prepend("<li>" + [index] + "</li>");
+  for (var index = 1; index <= stopInput; index ++) {
+    if (index % 15 === 0) {
+      $("ul.output").append("<li>ping-pong</li>");
+    }
+    else if (index % 3 === 0) {
+      $("ul.output").append("<li>ping</li>");
+    }
+    else if (index % 5 === 0) {
+      $("ul.output").append("<li>pong</li>");
+    }
+    else {
+      $("ul.output").append("<li>" + index + "</li>");
+    }
   }
-
 });
 
 // To tell if one number is divisible by another number, use the % operator. For example, if a variable called number is divisible by 7, then number % 7 returns 0.
