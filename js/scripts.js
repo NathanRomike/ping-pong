7ca2f6bb1165d24db55050df0c23d7c0d7786889
NathanRomike <nathanromike@gmail.com>
2015-10-16 20:44:56 -0700

    Add bootstrap, add array.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(function(){
+
+  var stop = parseInt(prompt("What number would you like to ping-pong up to?"));
+
+  var number = ["stop"].indexOf
+
+  for (var index = 1; index <= .stop; index += 1) {
+    $("ul").prepend("<li>" + [index] + "</li>");
+  }
+
+});
+
+// To tell if one number is divisible by another number, use the % operator. For example, if a variable called number is divisible by 7, then number % 7 returns 0.
