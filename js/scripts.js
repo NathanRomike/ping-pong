ee6696dee178f6358cff5e0df9ae0dd0c2d7c0bc
NathanRomike <nathanromike@gmail.com>
2015-10-17 00:04:30 -0700

    Correct spacing.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(function() {
   var endInput = parseInt(prompt("What number would you like to ping-pong up to?"));
   for (var index = 1; index <= endInput; index ++) {
-    if ((index % 3  === 0) && (index % 5 === 0)) {
+    if ((index % 3 === 0) && (index % 5 === 0)) {
       $("ul").append("<li>ping-pong</li>");
     }
     else if (index % 3 === 0) {
